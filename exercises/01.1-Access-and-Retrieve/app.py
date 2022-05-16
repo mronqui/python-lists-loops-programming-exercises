fd676ab6258fe03eef4d91d14d7066800a05f5fe
Mauricio Ronqui <mronqui@gmail.com>
2022-05-16 18:30:15 +0000

    ejercicios

--- exercises/01.1-Access-and-Retrieve/app.py
+++ exercises/01.1-Access-and-Retrieve/app.py
@@ -2,7 +2,14 @@
 my_list = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
 
 # 1. print the item here
-
+print(my_list[2])
 # 2. change the position were 'thursday' is to None
+my_list[2] = 'None'
+ # for i in range(0,len(my_list)):
+    #  if my_list[i] == 'tuesday'
+       #   my_list[i] = 'None'
+ #   print("La posicion es "+str(i)+" para el elemento "+myList[i])
+
 
 # 3. print that position now here
+print(my_list[2])
--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,10 @@
 #Remember import random function here:
+import random
 
 my_list = [4,5,734,43,45]
 
 #The magic is here:
+for i in range(0,10):
+    my_list.append(random.randint(100,10000))
+print(my_list) 
+
